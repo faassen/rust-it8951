a5fbd5e2804299e46fdf2ad680dee6cb09855e37
Martijn Faassen <faassen@startifact.com>
2020-11-23 14:40:08 +0100

    It does something at all.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use bincode::config::Options;
+use rusb::{DeviceHandle, GlobalContext};
 use serde::{Deserialize, Serialize};
+use std::time::Duration;
 
 mod usb;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Start");
+    let mut device_handle = usb::open_it8951().expect("Cannot open it8951");
+    device_handle
+        .set_auto_detach_kernel_driver(true)
+        .expect("auto detached failed");
+    device_handle.claim_interface(0).expect("claim failed");
+    inquiry(&device_handle);
+    device_handle.release_interface(0).expect("release failed");
+    println!("End");
 }
 
 #[repr(C)]
@@ -53,8 +63,47 @@ struct IT8951_display_area {
     wait_ready: i32,
 }
 
+const INQUIRY_CMD: [u8; 16] = [0x12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+struct InquiryResult {
+    vendor: [u8; 8],
+    product: [u8; 16],
+    revision: [u8; 4],
+}
+
+// XXX can I hardcode these?
+// There is code to obtain these and check whether they are in and out
+// endpoints
+const ENDPOINT_IN: u8 = 0x81;
+const ENDPOINT_OUT: u8 = 0x02;
+
+fn inquiry(device_handle: &DeviceHandle<GlobalContext>) -> InquiryResult {
+    usb::send_mass_storage_command(
+        device_handle,
+        ENDPOINT_OUT,
+        INQUIRY_CMD,
+        36,
+        usb::Direction::IN,
+    )
+    .expect("Cannot send inquiry command");
+    let mut buf: [u8; 256] = [0; 256];
+    let size = device_handle
+        .read_bulk(ENDPOINT_IN, &mut buf, Duration::from_millis(1000))
+        .expect("failed to bulk read");
+    println!("size {}", size);
+    InquiryResult {
+        vendor: [0, 0, 0, 0, 0, 0, 0, 0],
+        product: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        revision: [0, 0, 0, 0],
+    }
+}
+
 const LOAD_IMAGE_CMD: [u8; 16] = [
     0xfe, 0x00, 0x00, 0x00, 0x00, 0x00, 0xa2, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+const DISPLAY_AREA_CMD: [u8; 16] = [
+    0xfe, 0x00, 0x00, 0x00, 0x00, 0x00, 0x94, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ];
 
 fn load_image_area(f: i32, addr: i32, x: i32, y: i32, w: i32, h: i32, data: &[u8]) {
--- src/usb.rs
+++ src/usb.rs
@@ -33,34 +33,86 @@ struct CommandStatusWrapper {
     status: u8,
 }
 
-static TAG: AtomicU32 = AtomicU32::new(0);
+static TAG: AtomicU32 = AtomicU32::new(1);
 
 pub fn open_it8951() -> Option<DeviceHandle<GlobalContext>> {
-    open_device_with_vid_pid(0x1B3F, 0x30FE)
+    open_device_with_vid_pid(0x48d, 0x8951)
 }
 
-pub fn send_mass_storage_command(
-    device_handle: DeviceHandle<GlobalContext>,
-    endpoint: u8,
+pub fn get_mass_storage_command_data(
     command_data: [u8; 16],
     data_transfer_length: u32,
     direction: Direction,
-) -> Result<usize> {
+) -> Vec<u8> {
     let flags: u8 = match direction {
         Direction::IN => 0x80,
         Direction::OUT => 0x00,
         Direction::NONE => 0x00,
     };
+    let tag = TAG.fetch_add(1, Ordering::SeqCst);
     let cwb = CommandBlockWrapper {
         signature: [0x55, 0x53, 0x42, 0x43],
-        tag: TAG.fetch_add(1, Ordering::SeqCst),
+        tag: tag,
         data_transfer_length: data_transfer_length,
         flags: flags,
         logical_unit_number: 0,
         command_length: 16,
         command_data: command_data,
     };
-    let data_buffer: Vec<u8> = bincode::DefaultOptions::new().serialize(&cwb).unwrap();
+    bincode::options()
+        .with_little_endian()
+        .with_fixint_encoding()
+        .serialize(&cwb)
+        .unwrap()
+}
 
-    return device_handle.write_bulk(endpoint, &data_buffer, Duration::from_millis(1000));
+pub fn send_mass_storage_command(
+    device_handle: &DeviceHandle<GlobalContext>,
+    endpoint: u8,
+    command_data: [u8; 16],
+    data_transfer_length: u32,
+    direction: Direction,
+) -> Result<usize> {
+    let data = &get_mass_storage_command_data(command_data, data_transfer_length, direction);
+    // println!(
+    //     "data: {:?} len: {}, endpoint: {}",
+    //     data,
+    //     data.len(),
+    //     endpoint
+    // );
+    return device_handle.write_bulk(endpoint, data, Duration::from_millis(1000));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn mass_storage_command_data() {
+        let data = get_mass_storage_command_data(
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+            18,
+            Direction::OUT,
+        );
+        assert_eq!(
+            data,
+            [
+                85, 83, 66, 67, 0, 0, 0, 0, 18, 0, 0, 0, 0, 0, 16, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+                10, 11, 12, 13, 14, 15
+            ]
+        );
+        // the second time the tag increases
+        let data2 = get_mass_storage_command_data(
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+            18,
+            Direction::OUT,
+        );
+        assert_eq!(
+            data2,
+            [
+                85, 83, 66, 67, 1, 0, 0, 0, 18, 0, 0, 0, 0, 0, 16, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+                10, 11, 12, 13, 14, 15
+            ]
+        )
+    }
 }
